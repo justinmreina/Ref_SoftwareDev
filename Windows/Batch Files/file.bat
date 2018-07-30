5220bd5ff83f87a09315be77d938411f35c74abd
Justin Reina <justinmreina@gmail.com>
2018-07-30 08:46:19 -0700

    (+)    initial commit

---
+++ Windows/Batch Files/file.bat
@@ -0,0 +1,24 @@
+:: @open 	File Header
+:: @date 	10/17/17
+:: @rev 	1
+
+:: @open 	Refs Listing
+
+
+:: @open 	List Directory Contents
+
+
+:: @open 	Check if File Exists
+
+
+:: @open 	Create File
+
+
+:: @open 	Remove File
+
+
+:: @open 	Move File
+
+:: @section 	Opens
+::		move this to co-exist on Work & Home computers, shared by copy like other files
+
